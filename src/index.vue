388415d6cafa4704244d3341c5e95d068e9e1f16
Shruan <qiushiyuan1994@qq.com>
2017-10-24 00:40:27 +0800

    移除.vue根文件内容

--- src/index.vue
+++ src/index.vue
@@ -1,37 +1,29 @@
 <template>
-  <div class="wrapper" id="root" @click="update">
+  <div id="root"  class="wrapper">
     <router-view></router-view>
   </div>
 </template>
 
-<style>
-  .wrapper { align-items: center; margin-top: 120px; }
-  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
-  .logo { width: 360px; height: 156px; }
-  .desc { padding-top: 20px; color:#888; font-size: 24px;}
-</style>
-
 <script>
-  // import config from'./main.js'
 
-  // var config = {}
   export default {
-    data: {
-      logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
-      target: 'World'
+    data () {
+      return {
+
+      }
     },
     created () {
-      console.log('xxxxxx')
-      console.log(this)
-      this.$router.push({path: '/login'})
+
     },
     mounted () {
+
     },
     methods: {
-      update: function (e) {
-        this.target = 'Weex'
-        console.log('target:', this.target)
-      }
+
     }
   }
 </script>
+
+<style scoped>
+
+</style>
