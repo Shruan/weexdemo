b660e5d3a5cdf5729cccf7ee7e863a72102e28f0
Shruan <qiushiyuan1994@qq.com>
2017-10-24 00:48:47 +0800

    vue-router测试页面

---
+++ src/pages/test/test1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper" @click="update">
+    <image :src="logoUrl" class="logo"></image>
+    <p>test1</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
+        target: 'World'
+      }
+    },
+    created () {
+      console.log('test')
+    },
+    methods: {
+      update: function (e) {
+        this.target = 'Weex'
+        console.log('target:', this.target)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
