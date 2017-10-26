71b3575d67b39c242089bbe2ef389f5f19fcc77d
Shruan <qiushiyuan1994@qq.com>
2017-10-26 14:40:29 +0800

    引入请求js，区分登录平台（web或app）

--- src/main.js
+++ src/main.js
@@ -6,7 +6,17 @@ import store from '../src/store/index.js';
 import axios from 'axios';
 import Vuex from 'vuex';
 
-Vue.prototype.$http = axios
+import { appRequire, myAxios } from './assets/require.js'
+
+const modal = weex.requireModule('modal')
+if (WXEnvironment.platform == 'Web') {
+  modal.toast({ message: 'web端' })
+  Vue.prototype.$http = myAxios
+} else {
+  modal.toast({ message: 'app' })
+  Vue.prototype.$http = appRequire
+}
+
 Vue.prototype.apiUrl = 'http://112.74.31.159:8233/smy-worker-web'
 
 new Vue(
